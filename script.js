493a35b7cebe6992ec5608b3ed321d43c5c32b75
Aymende7 <aymenbousselham@gmail.com>
2021-06-18 13:15:32 +0100

    Password generator: Style polish
    * thanks to @soufianemch18 for the layout
    * thanks to Priler for the background animation codes
    * icons by icon-icons.com

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-function Generate(){
-//   let dict = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
- let dict='';
-  let lower = 'abcdefghijklmnopqrstuvwxyz';
-  let upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let numbers = '0123456789';
-  let symbols = '+$%{;*! @';
-  if(this.document.getElementById("lowercase").checked) dict += lower;
-  if(this.document.getElementById("uppercase").checked) dict += upper;
-  if(this.document.getElementById("numbers").checked) dict += numbers;
-  if(this.document.getElementById("symbols").checked) dict += symbols;
-  let password =''
+function Generate() {
+  let dict = "";
+  let lower = "abcdefghijklmnopqrstuvwxyz";
+  let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let numbers = "0123456789";
+  let symbols = "+$%{;*! @";
+  if (this.document.getElementById("lowercase").checked) dict += lower;
+  if (this.document.getElementById("uppercase").checked) dict += upper;
+  if (this.document.getElementById("numbers").checked) dict += numbers;
+  if (this.document.getElementById("symbols").checked) dict += symbols;
+  let password = "";
   var x = this.document.getElementById("length");
-  for (let i =0; i < x.value;i++){
-      password = password + dict.charAt(Math.floor(Math.random()*dict.length));
+  for (let i = 0; i < x.value; i++) {
+    password = password + dict.charAt(Math.floor(Math.random() * dict.length));
   }
   var pass_element = document.getElementById("pass");
-  pass_element.value=password;
+  pass_element.innerHTML = password;
   console.log(password);
 }
 
-window.addEventListener('load', function () {
+window.addEventListener("load", function () {
   var x = this.document.getElementById("length");
   for (let i =8;i < 17;i++){
     var option = this.document.createElement("option");
     option.text=i;
     x.add(option);
   }
-  this.document.getElementById("lowercase").checked=true;
-})
-
+  this.document.getElementById("lowercase").checked = true;
+});
